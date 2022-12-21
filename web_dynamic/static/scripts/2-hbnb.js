b2414bb340b094534c20b166bcc43d4f4253a3a3
root <piotr62@live.fr>
2022-12-20 23:35:53 -0800

    fixing and updating script 2 for task 3 API status// need a co-worker with port 5001 available // can't find out how to configure it

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,22 +1,25 @@
 $(document).ready(function () {
-  let amenityId = [];
-  $('input:checkbox').change(function () {
+  const amens = {};
+  $('input:checkbox').click(function () {
     if ($(this).is(':checked')) {
-      amenityId.push($(this).data('name'));
-      $('.amenities h4').text(amenityId.join(', '));
+      amens[$(this).data('id')] = $(this).data('name');
     } else {
-      amenityId = amenityId.filter(amens => amens !== amenName);
-      $('.amenities h4').text(amenityId.join(', '));
+      delete amens[$(this).data('id')];
+    }
+    if (Object.values(amens).length > 0) {
+      $('.amenities h4').text(Object.values(amens).join(', '));
+    } else {
+      $('.amenities h4').html('&nbsp');
     }
   });
-  /****************************
- display status with a colored circle on right top of page
- *****************************************/
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-    if (data.status === 'OK') {
-      $('#api_status').addClass('available');
-    } else {
-      $('#api_status').removeClass('available');
-    }
-  });
+});
+
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+  console.log(data);
+  // alert(status);
+  if (data.status === 'OK') {
+    $('div#api_status').addClass('available');
+  } else {
+    $('div#api_status').removeClass('available');
+  }
 });
