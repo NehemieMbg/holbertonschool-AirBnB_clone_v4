32f1ee2db60803a8e86f8020bcee6893d6aa5cd8
NekodaMushi <4252@holbertonschool.com>
2022-12-22 08:00:17 +0000

    Task 5

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+  let amens = {};
+  $("input:checkbox").click(function () {
+    if ($(this).is(":checked")) {
+      const data = $(this).data();
+      amens[data.id] = data.name;
+    } else {
+      delete amens[$(this).data("id")];
+    }
+    if (Object.values(amens).length > 0) {
+      $(".amenities h4").text(Object.values(amens).join(", "));
+    } else {
+      $(".amenities h4").text("");
+    }
+  });
+  $("button").click(function (event) {
+    event.preventDefault();
+    $("article").remove();
+    $.ajax({
+      type: "POST",
+      url: "http://127.0.0.1:5001/api/v1/places_search/",
+      data: JSON.stringify({ amenities: Object.keys(amens) }),
+      dataType: "json",
+      contentType: "application/json",
+      success: function (places) {
+        $.ajax({
+          type: "GET",
+          url: "http://127.0.0.1:5001/api/v1/users/",
+          data: {},
+          dataType: "json",
+          contentType: "application/json",
+          success: function (users) {
+            amenities_post(places, users);
+          },
+        });
+      },
+    });
+  });
+});
+
+function amenities_post(places, users) {
+  let usergave = "";
+  places.forEach((place) => {
+    usergave = "";
+    users.forEach((user) => {
+      if (user.id === place.user_id) {
+        usergave = user.first_name + " " + user.last_name;
+      }
+    });
+    const guest = place.max_guest !== 1 ? "s" : "";
+    const rooms = place.number_rooms !== 1 ? "s" : "";
+    const bathrooms = place.number_bathrooms !== 1 ? "s" : "";
+    const html = `<article>
+	<div class="title_box">
+		<h2>${place.name}</h2>
+		<div class="price_by_night">$${place.price_by_night}</div>
+	</div>
+	<div class="information">
+		<div class="max_guest">${place.max_guest} Guest${guest}</div>
+		<div class="number_rooms">${place.number_rooms} Bedroom${rooms}</div>
+		<div class="number_bathrooms">${place.number_bathrooms} Bathroom${bathrooms}</div>
+	</div>
+	<div class="user">
+		<b>Owner:</b> ${usergave} 
+	</div>
+	<div class="description">
+		${place.description}
+	</div>
+	</article>`;
+  $(".places").append(html);
+});
+}
+
+$("button").click(function (interaction) {
+  interaction.preventDefault();
+  $("article").remove();
+  call();
+});
